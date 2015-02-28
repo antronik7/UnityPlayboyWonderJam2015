6ce944648db6e402c7dfca7841f49962fc7c32ec
Tinaud <mathieu.naud@hotmail.com>
2015-02-27 20:28:12 -0500

    Menu

---
+++ UnityPlayboy/Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour 
+{
+    public Font FontName;
+
+    void OnGUI()
+    {
+        GUIStyle StyleName = new GUIStyle();
+        StyleName.font = FontName;
+
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 4 + 30, 200, 50), "Jouer", StyleName))
+            Debug.Log("Bouton \"Jouer\" appuyé! :D");
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2, 350, 50), "Contrôles", StyleName))
+            Debug.Log("Bouton \"Contrôles\" appuyé! :D");
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, 3 * Screen.height / 4 - 30, 260, 50), "Quitter", StyleName))
+            Debug.Log("Bouton \"Quitter\" appuyé! :D");
+    }
+}
