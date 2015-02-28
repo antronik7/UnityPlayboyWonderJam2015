02f1cadd7b73df80001c4be4f3c6f52a185c81e6
Tinaud <mathieu.naud@hotmail.com>
2015-02-28 00:34:22 -0500

    Suite Phrases qui montent

--- UnityPlayboy/Assets/Scripts/ChoixPhrase.cs
+++ UnityPlayboy/Assets/Scripts/ChoixPhrase.cs
@@ -4,17 +4,29 @@ using System.Collections;
 public class ChoixPhrase : MonoBehaviour
 {
     public int PositionY;
-	void Start () 
+    public bool AnimationFin;
+    public GUIText Texte;
+
+    void Start()
     {
-        PositionY = 0;
-	}
-	
-	void FixedUpdate () 
+        Texte = GetComponentInParent<GUIText>();
+        Texte.text = "Autre phrase\n2\n3";
+    }
+
+	void Update () 
     {
-        if (Input.GetKeyDown("down") && PositionY != -2)
-            PositionY--;
-        if (Input.GetKeyDown("up") && PositionY != 0)
-            PositionY++;
-        transform.position = new Vector3(0, PositionY, 0);
-	}
+        if (transform.position.y == 0.6)
+        {
+            AnimationFin = true;
+            PositionY = 0;
+        }
+        if (AnimationFin)
+        {
+            if (Input.GetKeyDown("down") && PositionY != -2)
+                PositionY--;
+            if (Input.GetKeyDown("up") && PositionY != 0)
+                PositionY++;
+            transform.position = new Vector3(-2, PositionY, 0);
+        } 
+    }
 }
