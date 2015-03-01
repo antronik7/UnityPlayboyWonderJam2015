64a114bc8a5b30eba26251d8de04ef3795bd96b7
xdsalut <cristianlavoie111@hotmail.com>
2015-03-01 15:06:18 -0500

    no message

--- UnityPlayboy/Assets/Scripts/Menu.cs
+++ UnityPlayboy/Assets/Scripts/Menu.cs
@@ -4,17 +4,31 @@ using System.Collections;
 public class Menu : MonoBehaviour 
 {
     public Font FontName;
-	
     void OnGUI()
     {
-        GUIStyle StyleName = new GUIStyle();
-        StyleName.font = FontName;
+		GUIStyle StyleName = new GUIStyle ();
+		StyleName.font = FontName;
+		StyleName.fontSize = (20);
+		StyleName.normal.textColor = Color.white;
 
-        if (GUI.Button (new Rect (Screen.width / 2 - 50, Screen.height / 4 + 30, 200, 50), "Jouer", StyleName))
-			Application.LoadLevel ("MainAntoine");
-        if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2, 350, 50), "Contrôles", StyleName))
-					transform.position = new Vector3(0,0,-11);
-        if (GUI.Button(new Rect(Screen.width / 2 - 50, 3 * Screen.height / 4 - 30, 260, 50), "Quitter", StyleName))
-			Application.Quit();
-    }
+		if (transform.position.y == 0) {
+			if (GUI.Button (new Rect (Screen.width / 2 - 70, Screen.height / 4 + 220, 200, 50), "Jouer", StyleName)) {
+				Application.LoadLevel ("MainAntoine");
+			}
+			if (GUI.Button (new Rect (Screen.width / 2 - 110, Screen.height / 4 + 270, 350, 50), "Contrôles", StyleName))
+				transform.position = new Vector3 (0, 8, -11);
+			if (GUI.Button (new Rect (Screen.width / 2 - 140, Screen.height / 4 + 320, 350, 50), "Instructions", StyleName))
+				transform.position = new Vector3 (0, -8, -11);
+			if (GUI.Button (new Rect (Screen.width / 2 - 90, Screen.height / 4 + 370, 260, 50), "Quitter", StyleName))
+				Application.Quit ();
+		} else if (transform.position.y == 8) {
+			if (GUI.Button (new Rect (Screen.width / 2 - 80, Screen.height-45, 350, 50), "Retour", StyleName))
+				transform.position = new Vector3 (0, 0, -11);
+		} else
+		{
+            if (GUI.Button(new Rect(Screen.width / 2 - 80, Screen.height - 45, 350, 50), "Retour", StyleName))
+				transform.position = new Vector3 (0, 0, -11);
+		}
+	}
+
 }
